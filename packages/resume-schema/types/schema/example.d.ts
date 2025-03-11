f1c2372a2268e99a66874825c7e2fb4929ef64c7
lianqq <2088486180@qq.com>
2025-03-11 21:46:46 +0800

    fix: :bug: 成功修复样式问题

--- packages/resume-schema/types/schema/example.d.ts
+++
@@ -1,2 +0,0 @@
-import { ResumeData } from '../index';
-export declare const Simple: ResumeData;
--- packages/resume-schema/types/schema/index.d.ts
+++
@@ -1,4 +0,0 @@
-import { ResumeData, TemplateKey } from '../index';
-import { Simple } from './example';
-export declare const getResumeTemplate: (template: TemplateKey) => ResumeData;
-export { Simple };
